634b7ba01d519fb0575b4625aa423efd29c12a68
sumanth1308 <sumanthloves@gmail.com>
2018-11-13 14:24:41 +0530

    Add files via upload

---
+++ Ex-2.py
@@ -0,0 +1,8 @@
+"""
+Exercise 2:Hello
+Write a program that asks the user to enter his or her name.
+The program should respond with a message that says hello to the user, using his or her name
+"""
+if __name__ == '__main__':
+    _name = raw_input("please enter your name:")
+    print 'Hello',_name
